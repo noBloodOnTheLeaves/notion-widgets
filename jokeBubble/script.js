ab8b32205bec227035e0ccd65afce35d704718e5
Igor <58074809+noBloodOnTheLeaves@users.noreply.github.com>
2023-10-02 00:52:25 +0300

    add-joke-bubble

---
+++ jokeBubble/script.js
@@ -0,0 +1,24 @@
+const test = async () => {
+	const response = await fetch("https://v2.jokeapi.dev/joke/Programming,Dark?blacklistFlags=political,racist,sexist", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7",
+    "sec-ch-ua": "\"Google Chrome\";v=\"117\", \"Not;A=Brand\";v=\"8\", \"Chromium\";v=\"117\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site"
+  },
+  "referrer": "https://sv443.net/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "omit"
+});
+	const movies = await response.json();
+	document.getElementById('joke').innerHTML(movies.joke)
+	console.log(movies)
+}
+test();
